37ee4989853bd88477bd54c19561549e28d7f19a
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-04 06:47:03 +0700

    menambahkan garfik analisis

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,11 +4,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Admin extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_pendaftaran');
+	}
 
 	public function index()
 	{
 		$data = array(
 			'title' => 'Dashboard',
+			'grafik' => $this->m_pendaftaran->grafik(),
 			'isi' => 'v_admin'
 		);
 		$this->load->view('layout/backend/v_wrapper', $data, FALSE);
