9a12dd4aaa2cdc06d3ccc79608d01339a0f6c640
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-03 23:53:05 +0700

    membuat footer backend

---
+++ application/views/layout/backend/v_footer.php
@@ -0,0 +1,39 @@
+<!-- footer -->
+<div class="container-fluid">
+	<div class="footer">
+		<p>Copyright © 2018 Designed by html.design. All rights reserved.<br><br>
+			Distributed By: <a href="https://themewagon.com/">ThemeWagon</a>
+		</p>
+	</div>
+</div>
+</div>
+<!-- end dashboard inner -->
+</div>
+</div>
+</div>
+<!-- jQuery -->
+<script src="<?= base_url() ?>backend/js/jquery.min.js"></script>
+<script src="<?= base_url() ?>backend/js/popper.min.js"></script>
+<script src="<?= base_url() ?>backend/js/bootstrap.min.js"></script>
+<!-- wow animation -->
+<script src="<?= base_url() ?>backend/js/animate.js"></script>
+<!-- select country -->
+<script src="<?= base_url() ?>backend/js/bootstrap-select.js"></script>
+<!-- owl carousel -->
+<script src="<?= base_url() ?>backend/js/owl.carousel.js"></script>
+<!-- chart js -->
+<script src="<?= base_url() ?>backend/js/Chart.min.js"></script>
+<script src="<?= base_url() ?>backend/js/Chart.bundle.min.js"></script>
+<script src="<?= base_url() ?>backend/js/utils.js"></script>
+<script src="<?= base_url() ?>backend/js/analyser.js"></script>
+<!-- nice scrollbar -->
+<script src="<?= base_url() ?>backend/js/perfect-scrollbar.min.js"></script>
+<script>
+	var ps = new PerfectScrollbar('#sidebar');
+</script>
+<!-- custom js -->
+<script src="<?= base_url() ?>backend/js/custom.js"></script>
+<script src="<?= base_url() ?>backend/js/chart_custom_style1.js"></script>
+</body>
+
+</html>
