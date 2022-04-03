ace1a919f291a4936a10ced85060dcc21800f826
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-04 06:48:07 +0700

    menambahkan script crud pendaftaran

--- application/models/M_pendaftaran.php
+++ application/models/M_pendaftaran.php
@@ -12,6 +12,16 @@ class M_pendaftaran extends CI_Model
 		return $this->db->get()->result();
 	}
 
+	public function data_penerimaan()
+	{
+		$id_warga = $this->session->userdata('id_warga');
+		$this->db->select('*');
+		$this->db->from('pendaftaran');
+		$this->db->join('warga', 'pendaftaran.id_warga = warga.id_warga', 'left');
+		$this->db->where('pendaftaran.id_warga=', $id_warga);
+		return $this->db->get()->result();
+	}
+
 	public function dataterima()
 	{
 		$this->db->select('*');
@@ -47,5 +57,15 @@ class M_pendaftaran extends CI_Model
 	{
 		$this->db->where('id_pendaftaran', $data['id_pendaftaran']);
 		$this->db->update('pendaftaran', $data);
+	}
+
+	public function grafik()
+	{
+		$this->db->select_sum('alamat');
+		$this->db->select('pendaftaran.alamat');
+		$this->db->from('pendaftaran');
+		$this->db->order_by('pendaftaran.id_pendaftaran');
+		$this->db->order_by('alamat', 'desc');
+		return $this->db->get()->result();
 	}
 }
