6024122d43383b1723ab597094abe13625c411fc
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-03 23:47:00 +0700

    membuat view head frontend

---
+++ application/views/layout/frontend/v_head.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+
+<head>
+	<!-- Meta Tags -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="Site keywords here">
+	<meta name="description" content="">
+	<meta name='copyright' content=''>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Title -->
+	<title>Eduland &minus; Education & Courses HTML5 Template</title>
+
+	<!-- Favicon -->
+	<link rel="icon" type="<?= base_url() ?>frontend/image/png" href="images/favicon.png">
+
+	<!-- Web Font -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/bootstrap.min.css">
+	<!-- Font Awesome CSS -->
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/font-awesome.min.css">
+	<!-- Nice Select CSS -->
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/niceselect.css">
+	<!-- Fancy Box CSS -->
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/jquery.fancybox.min.css">
+	<!-- Fancy Box CSS -->
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/cube-portfolio.min.css">
+	<!-- Owl Carousel CSS -->
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/owl.carousel.min.css">
+	<!-- Animate CSS -->
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/animate.min.css">
+	<!-- Slick Nav CSS -->
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/slicknav.min.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/magnific-popup.css">
+
+	<!-- Eduland Stylesheet -->
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/normalize.css">
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/style.css">
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/responsive.css">
+
+	<!-- Eduland Colors -->
+	<link rel="stylesheet" href="<?= base_url() ?>frontend/css/colors/color1.css">
+
+</head>
