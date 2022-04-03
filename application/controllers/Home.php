3fd4f56dfa05b4bd4b97edcdd51dea56d9d297cb
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-04 06:48:46 +0700

    mengubah tampilan frontend home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,11 +4,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_program');
+	}
 
 	public function index()
 	{
 		$data = array(
 			'title' => 'Home',
+			'program' => $this->m_program->program_pkmb(),
 			'isi' => 'v_home'
 		);
 		$this->load->view('layout/frontend/v_wrapper', $data, FALSE);
