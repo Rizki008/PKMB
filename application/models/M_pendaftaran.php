b688d3b8f13672b84adb2066d6ebb923b3a97243
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-04 11:07:14 +0700

    menambahkan scrpit notifikasi

--- application/models/M_pendaftaran.php
+++ application/models/M_pendaftaran.php
@@ -61,11 +61,48 @@ class M_pendaftaran extends CI_Model
 
 	public function grafik()
 	{
-		$this->db->select_sum('alamat');
-		$this->db->select('pendaftaran.alamat');
+		$this->db->select_sum('desa');
+		$this->db->select('pendaftaran.desa');
 		$this->db->from('pendaftaran');
 		$this->db->order_by('pendaftaran.id_pendaftaran');
-		$this->db->order_by('alamat', 'desc');
+		$this->db->order_by('desa', 'desc');
+		return $this->db->get()->result();
+	}
+	public function grafik_usia()
+	{
+		$this->db->select_sum('usia');
+		$this->db->select('warga.usia');
+		$this->db->from('pendaftaran');
+		$this->db->join('warga', 'pendaftaran.id_warga = warga.id_warga', 'left');
+		$this->db->order_by('pendaftaran.id_pendaftaran');
+		$this->db->order_by('desa', 'desc');
+		return $this->db->get()->result();
+	}
+
+	public function notif()
+	{
+		$this->db->select('*');
+		$this->db->from('pendaftaran');
+		$this->db->group_by('id_pendaftaran');
+		return $this->db->get()->num_rows();
+	}
+
+	public function notifterima()
+	{
+		$this->db->select('*');
+		$this->db->from('pendaftaran');
+		$this->db->where('id_warga', $this->session->userdata('id_warga'));
+		$this->db->where('status=1');
+		$this->db->group_by('id_pendaftaran', 'desc');
+		return $this->db->get()->result();
+	}
+	public function notiftolak()
+	{
+		$this->db->select('*');
+		$this->db->from('pendaftaran');
+		$this->db->where('id_warga', $this->session->userdata('id_warga'));
+		$this->db->where('status=2');
+		$this->db->group_by('id_pendaftaran', 'desc');
 		return $this->db->get()->result();
 	}
 }
