4c795dc04b209705e924689ac373b2c722dd481c
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-09 11:41:33 +0700

    mengubah grafik analisis

--- application/views/v_admin.php
+++ application/views/v_admin.php
@@ -73,11 +73,11 @@
 
 		<!-- graph -->
 		<div class="row column2 graph margin_bottom_30">
-			<div class="col-md-l2 col-lg-12">
+			<div class="col-md-12 col-lg-12">
 				<div class="white_shd full">
 					<div class="full graph_head">
 						<div class="heading1 margin_0">
-							<h2>Grafik Analisis Sesuai desa</h2>
+							<h2>Grafik Analisis Kecamatan dan Usia</h2>
 						</div>
 					</div>
 					<div class="full graph_revenue">
@@ -97,7 +97,7 @@
 										<script>
 											var ctx = document.getElementById('myChart');
 											var myChart = new Chart(ctx, {
-												type: 'bar',
+												type: 'pie',
 												data: {
 													labels: <?= json_encode($kecamatan) ?>,
 													datasets: [{
@@ -185,38 +185,15 @@
 												},
 												options: {
 													scales: {
-														yAxes: [{
-															ticks: {
-																beginAtZero: true
-															}
-														}]
+														// yAxes: [{
+														// 	ticks: {
+														// 		beginAtZero: true
+														// 	}
+														// }]
 													}
 												}
 											});
 										</script>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- graph -->
-		<div class="row column2 graph margin_bottom_30">
-			<div class="col-md-l2 col-lg-12">
-				<div class="white_shd full">
-					<div class="full graph_head">
-						<div class="heading1 margin_0">
-							<h2>Grafik Analisis Sesuai Usia</h2>
-						</div>
-					</div>
-					<div class="full graph_revenue">
-						<div class="row">
-							<div class="col-md-12">
-								<div class="content">
-									<div class="area_chart">
 										<?php
 										foreach ($grafik_usia as $key => $value) {
 											$username[] = $value->username;
@@ -224,108 +201,109 @@
 										}
 										?>
 
-
-										<canvas id="myChart1" height="100"></canvas>
-										<script>
-											var ctx = document.getElementById('myChart1');
-											var myChart1 = new Chart(ctx, {
-												type: 'bar',
-												data: {
-													labels: <?= json_encode($username) ?>,
-													datasets: [{
-														label: 'Grafik Analisis Sesuai Usia',
-														data: <?= json_encode($usia) ?>,
-														backgroundColor: [
-															'rgba(255, 99, 132, 0.80)',
-															'rgba(54, 162, 235, 0.80)',
-															'rgba(255, 206, 86, 0.80)',
-															'rgba(75, 192, 192, 0.80)',
-															'rgba(153, 102, 255, 0.80)',
-															'rgba(255, 159, 64, 0.80)',
-															'rgba(201, 76, 76, 0.3)',
-															'rgba(201, 77, 77, 1)',
-															'rgba(0, 140, 162, 1)',
-															'rgba(158, 109, 8, 1)',
-															'rgba(201, 76, 76, 0.8)',
-															'rgba(0, 129, 212, 1)',
-															'rgba(201, 77, 201, 1)',
-															'rgba(255, 207, 207, 1)',
-															'rgba(201, 77, 77, 1)',
-															'rgba(128, 98, 98, 1)',
-															'rgba(0, 0, 0, 1)',
-															'rgba(128, 128, 128, 1)',
-															'rgba(255, 99, 132, 0.80)',
-															'rgba(54, 162, 235, 0.80)',
-															'rgba(255, 206, 86, 0.80)',
-															'rgba(75, 192, 192, 0.80)',
-															'rgba(153, 102, 255, 0.80)',
-															'rgba(255, 159, 64, 0.80)',
-															'rgba(201, 76, 76, 0.3)',
-															'rgba(201, 77, 77, 1)',
-															'rgba(0, 140, 162, 1)',
-															'rgba(158, 109, 8, 1)',
-															'rgba(201, 76, 76, 0.8)',
-															'rgba(0, 129, 212, 1)',
-															'rgba(201, 77, 201, 1)',
-															'rgba(255, 207, 207, 1)',
-															'rgba(201, 77, 77, 1)',
-															'rgba(128, 98, 98, 1)',
-															'rgba(0, 0, 0, 1)',
-															'rgba(128, 128, 128, 1)'
-														],
-														borderColor: [
-															'rgba(255, 99, 132, 1)',
-															'rgba(54, 162, 235, 1)',
-															'rgba(255, 206, 86, 1)',
-															'rgba(75, 192, 192, 1)',
-															'rgba(153, 102, 255, 1)',
-															'rgba(255, 159, 64, 1)',
-															'rgba(201, 76, 76, 0.3)',
-															'rgba(201, 77, 77, 1)',
-															'rgba(0, 140, 162, 1)',
-															'rgba(158, 109, 8, 1)',
-															'rgba(201, 76, 76, 0.8)',
-															'rgba(0, 129, 212, 1)',
-															'rgba(201, 77, 201, 1)',
-															'rgba(255, 207, 207, 1)',
-															'rgba(201, 77, 77, 1)',
-															'rgba(128, 98, 98, 1)',
-															'rgba(0, 0, 0, 1)',
-															'rgba(128, 128, 128, 1)',
-															'rgba(255, 99, 132, 1)',
-															'rgba(54, 162, 235, 1)',
-															'rgba(255, 206, 86, 1)',
-															'rgba(75, 192, 192, 1)',
-															'rgba(153, 102, 255, 1)',
-															'rgba(255, 159, 64, 1)',
-															'rgba(201, 76, 76, 0.3)',
-															'rgba(201, 77, 77, 1)',
-															'rgba(0, 140, 162, 1)',
-															'rgba(158, 109, 8, 1)',
-															'rgba(201, 76, 76, 0.8)',
-															'rgba(0, 129, 212, 1)',
-															'rgba(201, 77, 201, 1)',
-															'rgba(255, 207, 207, 1)',
-															'rgba(201, 77, 77, 1)',
-															'rgba(128, 98, 98, 1)',
-															'rgba(0, 0, 0, 1)',
-															'rgba(128, 128, 128, 1)'
-														],
-														fill: false,
-														borderWidth: 1
-													}]
-												},
-												options: {
-													scales: {
-														yAxes: [{
-															ticks: {
-																beginAtZero: true
-															}
+										<div class="area_chart">
+											<canvas id="myChart1" height="100"></canvas>
+											<script>
+												var ctx = document.getElementById('myChart1');
+												var myChart1 = new Chart(ctx, {
+													type: 'bar',
+													data: {
+														labels: <?= json_encode($username) ?>,
+														datasets: [{
+															label: 'Grafik Analisis Sesuai Usia',
+															data: <?= json_encode($usia) ?>,
+															backgroundColor: [
+																'rgba(255, 99, 132, 0.80)',
+																'rgba(54, 162, 235, 0.80)',
+																'rgba(255, 206, 86, 0.80)',
+																'rgba(75, 192, 192, 0.80)',
+																'rgba(153, 102, 255, 0.80)',
+																'rgba(255, 159, 64, 0.80)',
+																'rgba(201, 76, 76, 0.3)',
+																'rgba(201, 77, 77, 1)',
+																'rgba(0, 140, 162, 1)',
+																'rgba(158, 109, 8, 1)',
+																'rgba(201, 76, 76, 0.8)',
+																'rgba(0, 129, 212, 1)',
+																'rgba(201, 77, 201, 1)',
+																'rgba(255, 207, 207, 1)',
+																'rgba(201, 77, 77, 1)',
+																'rgba(128, 98, 98, 1)',
+																'rgba(0, 0, 0, 1)',
+																'rgba(128, 128, 128, 1)',
+																'rgba(255, 99, 132, 0.80)',
+																'rgba(54, 162, 235, 0.80)',
+																'rgba(255, 206, 86, 0.80)',
+																'rgba(75, 192, 192, 0.80)',
+																'rgba(153, 102, 255, 0.80)',
+																'rgba(255, 159, 64, 0.80)',
+																'rgba(201, 76, 76, 0.3)',
+																'rgba(201, 77, 77, 1)',
+																'rgba(0, 140, 162, 1)',
+																'rgba(158, 109, 8, 1)',
+																'rgba(201, 76, 76, 0.8)',
+																'rgba(0, 129, 212, 1)',
+																'rgba(201, 77, 201, 1)',
+																'rgba(255, 207, 207, 1)',
+																'rgba(201, 77, 77, 1)',
+																'rgba(128, 98, 98, 1)',
+																'rgba(0, 0, 0, 1)',
+																'rgba(128, 128, 128, 1)'
+															],
+															borderColor: [
+																'rgba(255, 99, 132, 1)',
+																'rgba(54, 162, 235, 1)',
+																'rgba(255, 206, 86, 1)',
+																'rgba(75, 192, 192, 1)',
+																'rgba(153, 102, 255, 1)',
+																'rgba(255, 159, 64, 1)',
+																'rgba(201, 76, 76, 0.3)',
+																'rgba(201, 77, 77, 1)',
+																'rgba(0, 140, 162, 1)',
+																'rgba(158, 109, 8, 1)',
+																'rgba(201, 76, 76, 0.8)',
+																'rgba(0, 129, 212, 1)',
+																'rgba(201, 77, 201, 1)',
+																'rgba(255, 207, 207, 1)',
+																'rgba(201, 77, 77, 1)',
+																'rgba(128, 98, 98, 1)',
+																'rgba(0, 0, 0, 1)',
+																'rgba(128, 128, 128, 1)',
+																'rgba(255, 99, 132, 1)',
+																'rgba(54, 162, 235, 1)',
+																'rgba(255, 206, 86, 1)',
+																'rgba(75, 192, 192, 1)',
+																'rgba(153, 102, 255, 1)',
+																'rgba(255, 159, 64, 1)',
+																'rgba(201, 76, 76, 0.3)',
+																'rgba(201, 77, 77, 1)',
+																'rgba(0, 140, 162, 1)',
+																'rgba(158, 109, 8, 1)',
+																'rgba(201, 76, 76, 0.8)',
+																'rgba(0, 129, 212, 1)',
+																'rgba(201, 77, 201, 1)',
+																'rgba(255, 207, 207, 1)',
+																'rgba(201, 77, 77, 1)',
+																'rgba(128, 98, 98, 1)',
+																'rgba(0, 0, 0, 1)',
+																'rgba(128, 128, 128, 1)'
+															],
+															fill: false,
+															borderWidth: 1
 														}]
+													},
+													options: {
+														scales: {
+															yAxes: [{
+																ticks: {
+																	beginAtZero: true
+																}
+															}]
+														}
 													}
-												}
-											});
-										</script>
+												});
+											</script>
+										</div>
 									</div>
 								</div>
 							</div>
