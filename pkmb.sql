ab95b35526880ff35f43dc380c2b544d28592ce4
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-18 00:25:02 +0700

    mengubah db baru

--- pkmb.sql
+++ pkmb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Apr 2022 pada 09.02
--- Versi server: 10.4.22-MariaDB
--- Versi PHP: 7.4.27
+-- Waktu pembuatan: 17 Bulan Mei 2022 pada 19.24
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 7.4.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,10 +29,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `kelas` (
   `id_kelas` int(11) NOT NULL,
-  `id_daftar` int(11) DEFAULT NULL,
-  `kelas` varchar(25) DEFAULT NULL,
-  `paket` varchar(25) DEFAULT NULL
+  `id_pendaftaran` int(11) DEFAULT NULL,
+  `kelas` varchar(25) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `id_pendaftaran`, `kelas`) VALUES
+(1, 1, 'c'),
+(2, NULL, 'c');
 
 -- --------------------------------------------------------
 
@@ -43,60 +50,25 @@ CREATE TABLE `kelas` (
 CREATE TABLE `pendaftaran` (
   `id_pendaftaran` int(11) NOT NULL,
   `id_warga` int(11) DEFAULT NULL,
-  `nama_lengkap` varchar(50) DEFAULT NULL,
-  `nama_pang` varchar(50) DEFAULT NULL,
-  `jenis_kel` varchar(25) DEFAULT NULL,
   `tempat_lahir` varchar(50) DEFAULT NULL,
   `tgl_lahir` date DEFAULT NULL,
   `agama` varchar(50) DEFAULT NULL,
-  `warganegara` varchar(50) DEFAULT NULL,
-  `anak_ke` varchar(25) DEFAULT NULL,
-  `jml_sdr` varchar(25) DEFAULT NULL,
-  `jml_sdr_tiri` varchar(25) DEFAULT NULL,
-  `jml_sdr_angkat` varchar(25) DEFAULT NULL,
-  `bahasa` varchar(50) DEFAULT NULL,
-  `berat_bd` varchar(25) DEFAULT NULL,
-  `tinggi_bd` varchar(25) DEFAULT NULL,
-  `gol_darah` varchar(25) DEFAULT NULL,
-  `penyakit` varchar(125) DEFAULT NULL,
-  `provinsi` varchar(50) DEFAULT NULL,
-  `rt_rw` varchar(50) DEFAULT NULL,
   `desa` varchar(50) DEFAULT NULL,
   `kecamatan` varchar(50) DEFAULT NULL,
   `kabupaten` varchar(50) DEFAULT NULL,
-  `no_tlpn` varchar(15) DEFAULT NULL,
-  `tempat_tinggal` text DEFAULT NULL,
-  `ayah` varchar(125) DEFAULT NULL,
-  `ibu` varchar(125) DEFAULT NULL,
-  `sklh_ayah` varchar(125) DEFAULT NULL,
-  `sklh_ibu` varchar(125) DEFAULT NULL,
-  `pkrj_ayah` varchar(125) DEFAULT NULL,
-  `pkrj_ibu` varchar(125) DEFAULT NULL,
-  `wali` varchar(125) DEFAULT NULL,
-  `sklh_tinggi` varchar(125) DEFAULT NULL,
-  `hub_peserta` varchar(125) DEFAULT NULL,
-  `pekerjaan` varchar(125) DEFAULT NULL,
-  `alamat_ortu` text DEFAULT NULL,
-  `tlpn` varchar(15) DEFAULT NULL,
-  `msk_sklh` varchar(125) DEFAULT NULL,
-  `nama_sklh` varchar(125) DEFAULT NULL,
   `thn_lulus` date DEFAULT NULL,
-  `sklh_asal` varchar(125) DEFAULT NULL,
-  `tgl_pindah` date DEFAULT NULL,
-  `kelas_pindah` varchar(25) DEFAULT NULL,
   `tgl_terima` date DEFAULT NULL,
-  `kelas_terima` varchar(25) DEFAULT NULL,
+  `pindahan` varchar(125) DEFAULT NULL,
   `status` varchar(50) DEFAULT NULL,
-  `alasan` text DEFAULT NULL
+  `alasan_diterima` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `pendaftaran`
 --
 
-INSERT INTO `pendaftaran` (`id_pendaftaran`, `id_warga`, `nama_lengkap`, `nama_pang`, `jenis_kel`, `tempat_lahir`, `tgl_lahir`, `agama`, `warganegara`, `anak_ke`, `jml_sdr`, `jml_sdr_tiri`, `jml_sdr_angkat`, `bahasa`, `berat_bd`, `tinggi_bd`, `gol_darah`, `penyakit`, `provinsi`, `rt_rw`, `desa`, `kecamatan`, `kabupaten`, `no_tlpn`, `tempat_tinggal`, `ayah`, `ibu`, `sklh_ayah`, `sklh_ibu`, `pkrj_ayah`, `pkrj_ibu`, `wali`, `sklh_tinggi`, `hub_peserta`, `pekerjaan`, `alamat_ortu`, `tlpn`, `msk_sklh`, `nama_sklh`, `thn_lulus`, `sklh_asal`, `tgl_pindah`, `kelas_pindah`, `tgl_terima`, `kelas_terima`, `status`, `alasan`) VALUES
-(1, 1, 'diana', 'dian', 'Perempuan', 'kuningan', '1999-06-09', 'islam', 'WNI', '1', '1', '', '', '', '50', '160', 'A', '', 'jawabarat', '12/04', 'ciawilor', 'ciawigebang', 'kuningan', '0897126273812', 'Orangtua', 'jamaludin', 'omah', 'sd', 'sd', 'buruh', 'ibu rumah tangga', '', '', '', '', 'Jakarta', '0891238727182', 'Peserta Didik Baru', 'sdn kuningan', '2019-02-04', '', '0000-00-00', '', '2022-04-06', NULL, '2', 'sangat pinter'),
-(2, 1, 'uud', 'jamal', 'Laki-Laki', 'kuningan', '2010-02-16', 'islam', 'WNI', '1', '2', '', '', '', '123', '123', 'AB', '', 'DKI', '12/04', 'ciawilor', 'ciawigebang', 'Kuningan', '0871234567', 'Orangtua', 'jamaludin', 'omah', 'sd', 'sd', 'buruh', 'ibu rumah tangga', '', '', '', '', 'sindang barang', '0891238727182', 'Peserta Didik Baru', 'sdn kuningan', '2022-04-04', '', '0000-00-00', '', '2022-04-16', 'C', '1', 'sasa');
+INSERT INTO `pendaftaran` (`id_pendaftaran`, `id_warga`, `tempat_lahir`, `tgl_lahir`, `agama`, `desa`, `kecamatan`, `kabupaten`, `thn_lulus`, `tgl_terima`, `pindahan`, `status`, `alasan_diterima`) VALUES
+(1, 3, 'kuningan', '2022-05-03', 'islam', 'ciawilor', 'ciawigebang', 'Kuningan', '2022-05-18', '2022-05-19', '', '1', 'pinter');
 
 -- --------------------------------------------------------
 
@@ -148,19 +120,23 @@ INSERT INTO `user` (`id_user`, `username`, `password`, `level`) VALUES
 
 CREATE TABLE `warga` (
   `id_warga` int(11) NOT NULL,
+  `nama_lengkap` varchar(125) DEFAULT NULL,
   `username` varchar(125) DEFAULT NULL,
   `password` varchar(125) DEFAULT NULL,
   `jenis_kel` varchar(20) DEFAULT NULL,
-  `usia` varchar(20) DEFAULT NULL
+  `usia` varchar(20) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `no_tlpn` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `warga`
 --
 
-INSERT INTO `warga` (`id_warga`, `username`, `password`, `jenis_kel`, `usia`) VALUES
-(1, 'diana', '12345', 'perempuan', '25'),
-(2, 'contoh', '12345', 'perempuan', '25');
+INSERT INTO `warga` (`id_warga`, `nama_lengkap`, `username`, `password`, `jenis_kel`, `usia`, `alamat`, `no_tlpn`) VALUES
+(1, 'diana safitri', 'diana', '12345', 'perempuan', '25', 'kuningan', '085741321452'),
+(2, 'intan sari', 'intan', '12345', 'perempuan', '25', 'sindang barang', '085156727368'),
+(3, 'riki afrizon', 'admin', '12345', 'laki-laki', '25', 'jalaksana', '085745698745');
 
 --
 -- Indexes for dumped tables
@@ -204,13 +180,13 @@ ALTER TABLE `warga`
 -- AUTO_INCREMENT untuk tabel `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `pendaftaran`
 --
 ALTER TABLE `pendaftaran`
-  MODIFY `id_pendaftaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pendaftaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `pro_pkmb`
@@ -228,7 +204,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `warga`
 --
 ALTER TABLE `warga`
-  MODIFY `id_warga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_warga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
