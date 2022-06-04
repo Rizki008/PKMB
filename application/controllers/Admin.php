082bd1f9fd31887e210f1c9ceda7dac0a15a02a1
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-04 20:49:53 +0700

    menambahkan proses analisis kelulusan di admin dan yayasan

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -12,6 +12,7 @@ class Admin extends CI_Controller
 
 	public function index()
 	{
+		$tahun = $this->input->post('tahun');
 		$data = array(
 			'title' => 'Dashboard',
 			'grafik' => $this->m_pendaftaran->grafik(),
@@ -22,6 +23,8 @@ class Admin extends CI_Controller
 			'grafik_usia' => $this->m_pendaftaran->grafik_usia(),
 			'gafik_genap' => $this->m_pendaftaran->gafik_genap(),
 			'gafik_ganjil' => $this->m_pendaftaran->gafik_ganjil(),
+			'tahun' => $tahun,
+			'kelulusan' => $this->m_pendaftaran->lulus_tahunan($tahun),
 			'isi' => 'v_admin'
 		);
 		$this->load->view('layout/backend/v_wrapper', $data, FALSE);
@@ -41,5 +44,18 @@ class Admin extends CI_Controller
 			'isi' => 'v_yayasan'
 		);
 		$this->load->view('layout/yayasan/v_wrapper', $data, FALSE);
+	}
+
+	public function lulus_tahunan()
+	{
+		$tahun = $this->input->post('tahun');
+
+		$data = array(
+			'title' => 'Data Kelulusan Pertahun',
+			'tahun' => $tahun,
+			'kelulusan' => $this->m_pendaftaran->lulus_tahunan($tahun),
+			'isi' => 'layout/backend/kelulusan/v_lulusan_tahunan'
+		);
+		$this->load->view('layout/backend/v_wrapper', $data, FALSE);
 	}
 }
