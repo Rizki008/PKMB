0d53cfe116f0d06058017dc250fa1d39135517ae
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-06-09 07:09:50 +0700

    menambahkan fitur menu analisis semua data

--- application/views/layout/yayasan/v_header.php
+++ application/views/layout/yayasan/v_header.php
@@ -28,18 +28,26 @@
 						</li>
 						<!-- <li><a href="<?= base_url('pendaftaran/yayasan') ?>"><i class="fa fa-clock-o orange_color"></i> <span>Pendaftaran Warga</span></a></li> -->
 						<li>
-							<a href="#element" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-diamond purple_color"></i> <span>Data Warga</span></a>
+							<a href="#element" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-group purple_color"></i> <span>Data Warga</span></a>
 							<ul class="collapse list-unstyled" id="element">
 								<li><a href="<?= base_url('pendaftaran/dataterima_yayasan') ?>">> <span>Data Diterima</span></a></li>
 								<li><a href="<?= base_url('pendaftaran/datatolak_yayasan') ?>">> <span>Data Ditolak</span></a></li>
 							</ul>
 						</li>
-						<li>
-							<a href="#elemente" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-diamond purple_color"></i> <span>Data Analisis Semester</span></a>
-							<ul class="collapse list-unstyled" id="elemente">
-								<li><a href="<?= base_url('admin/semester_genap') ?>">> <span>Semester Genap</span></a></li>
-								<li><a href="<?= base_url('admin/semester_ganjil') ?>">> <span>Semester Ganjil</span></a></li>
-							</ul>
+						<li class="active">
+							<a href="<?= base_url('analisis/semester') ?>"><i class="fa fa-calendar-check-o yellow_color"></i> <span>Analisis Semester</span></a>
+						</li>
+						<li class="active">
+							<a href="<?= base_url('analisis/alamat') ?>"><i class="fa fa-home blue2_color"></i> <span>Analisis Alamat</span></a>
+						</li>
+						<li class="active">
+							<a href="<?= base_url('analisis/lulusan') ?>"><i class="fa fa-graduation-cap green_color"></i> <span>Analisis Kelulusan</span></a>
+						</li>
+						<li class="active">
+							<a href="<?= base_url('analisis/usia') ?>"><i class="fa fa-bar-chart purple_color2"></i> <span>Analisis Usia</span></a>
+						</li>
+						<li class="active">
+							<a href="<?= base_url('analisis/paket') ?>"><i class="fa fa-book yellow_color"></i> <span>Analisis Kejar Paket</span></a>
 						</li>
 					</ul>
 				</div>
